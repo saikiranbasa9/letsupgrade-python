dfc78db04bcb71d43b14c638a5adbb71fa302925
saikiranbasa9 <68612234+saikiranbasa9@users.noreply.github.com>
2020-07-24 12:33:07 +0530

    Update assignment 1.py

--- assignment 1.py
+++ assignment 1.py
@@ -1,14 +1,14 @@
 print("1. Python is an interpreted, high-level, general-purpose programming language. Created by Guido van Rossum and first released in 1991, Python's design philosophy emphasizes code readability with its notable use of significant whitespace. Its language constructs and object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects.\
-2.Ways to install:- Go to Python.org website and download from there
-
-what are code environment?
-1.A code environment is a standalone and self-contained environment to run Python or R code. In each location where you can run Python or R code, you can select which code environment to use.
-2.Types of IDEs
-Multi-Language IDEs. ...
-IDEs for Mobile Development. ...
-HTML IDEs. ...
-Cloud-Based IDEs. ...
-IDEs Specific to Microsoft or Apple. ...
-IDEs for Specific Languages. ...
-Application security and the integrated development environment. ...
+2.Ways to install:- Go to Python.org website and download from there\
+\
+what are code environment?\
+1.A code environment is a standalone and self-contained environment to run Python or R code. In each location where you can run Python or R code, you can select which code environment to use.\
+2.Types of IDEs\
+Multi-Language IDEs. ...\
+IDEs for Mobile Development. ...\
+HTML IDEs. ...\
+Cloud-Based IDEs. ...\
+IDEs Specific to Microsoft or Apple. ...\
+IDEs for Specific Languages. ...\
+Application security and the integrated development environment. ...\
 Veracode solutions for the integrated development environment.")
